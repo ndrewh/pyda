8baa34062811b792933e0b70f42ecba165e4626e
Andrew Haberlandt <andrew.haberlandt@gmail.com>
2025-01-16 02:21:46 +0900

    wip: compiler

---
+++ lib/pyda/compiler/expr.py
@@ -0,0 +1,48 @@
+import pyda_core
+
+class Expr:
+    # it is NOT legal for user code to call this constructor EVER
+    def __init__(self, handle):
+        self._handle = handle
+    
+    def __add__(self, other):
+        other = self.expr_from(other)
+        return Expr(pyda_core.expr(pyda_core.EXPR_TYPE_ADD, self._handle, other._handle))
+    
+    def __sub__(self, other):
+        other = self.expr_from(other)
+        return Expr(pyda_core.expr(pyda_core.EXPR_TYPE_SUB, self._handle, other._handle))
+
+    def __mul__(self, other):
+        other = self.expr_from(other)
+        return Expr(pyda_core.expr(pyda_core.EXPR_TYPE_MUL, self._handle, other._handle))
+
+    def __floordiv__(self, other):
+        other = self.expr_from(other)
+        return Expr(pyda_core.expr(pyda_core.EXPR_TYPE_DIV, self._handle, other._handle))
+    
+    def __del__(self):
+        pyda_core.free_expr(self._handle)
+    
+    @staticmethod
+    def expr_from(val):
+        if isinstance(val, Expr):
+            return val
+        elif isinstance(val, int):
+            return Expr(pyda_core.expr(pyda_core.EXPR_TYPE_CONST, val, 0))
+        else:
+            raise ValueError(f"Invalid type for expression {type(val)}")
+
+
+class Builder:
+    def __init__(self):
+        pass
+
+    def load(self, addr):
+        return Expr(pyda_core.expr(pyda_core.EXPR_TYPE_LOAD, Expr.expr_from(addr).handle, 0))
+    
+    def store(self, addr, value):
+        return Expr(pyda_core.expr(pyda_core.EXPR_TYPE_STORE, Expr.expr_from(addr).handle, Expr.expr_from(value).handle))
+    
+    def constant(self, value):
+        return Expr(pyda_core.expr(pyda_core.EXPR_TYPE_CONST, value, 0))
